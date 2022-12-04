bb97273b00d17bd463f5a757c2fff018ee3b4bb7
Hugo Lindström <huggepugge1@gmail.com>
2022-12-04 13:11:46 +0100

    day 4 part 1

--- 2022/4/part1.py
+++ 2022/4/part1.py
@@ -1,6 +1,12 @@
 ans = 0
 with open("input.txt", "r") as f:
     while line := f.readline():
+        ranges = line[:-1].split(",")
+        ranges = [i.split("-") for i in ranges]
 
+        if int(ranges[0][0]) <= int(ranges[1][0]) and int(ranges[0][1]) >=int(ranges[1][1]):
+            ans += 1
+        elif int(ranges[1][0]) <= int(ranges[0][0]) and int(ranges[1][1]) >= int(ranges[0][1]):
+            ans += 1
 
 print(ans)
